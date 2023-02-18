4951979df4cb0079177d76433ddaa381d9ed22d1
UriProtocol <angel_3041210085@utd.edu.mx>
2023-02-18 01:17:10 -0600

    final

--- src/pages/Profesores.jsx
+++ src/pages/Profesores.jsx
@@ -4,8 +4,17 @@ import Modal from "react-bootstrap/Modal";
 import Table from "react-bootstrap/Table";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Profesores = () => {
+
+  const initialState = {
+    show: false,
+    clave: '',
+    nombres: '',
+    apellidos: ''
+ }
 
   const [datos, setDatos] = useState([]);
   const [show, setShow] = useState(false)
@@ -23,15 +32,58 @@ const Profesores = () => {
       .catch((error) => console.log(error));
   };
   const handleClose = () =>{
-    setShow(false)
+    setShow(initialState)
   }
-  const handleEliminar = () =>{
-    setShow(true)
+  const handleEliminar = (clave, nombre, apellidos) =>{
+    const showState = {
+      show: true,
+      clave,
+      nombre,
+      apellidos
+    }
+    setShow(showState)
   }
+
+  const handleEliminar2 = (clave) => {
+    axios.get(`http://localhost:5000/profesor/eliminar/${ clave }`)
+    .then(function(response){
+      if(response.data.result.affectedRows>0){
+        notify(response.status)
+        traerDatos();
+        setShow(initialState)
+      }
+    })
+    .catch(function(error) {
+      console.log(error)
+    })
+  }
+
+  function notify(num) {
+    if(num===200){
+      toast.error(
+        'Profesor eliminado',
+        {
+          position: toast.POSITION.TOP_CENTER,
+          onClose:() =>{
+          },
+          autoClose:800,
+          theme: 'colored'
+        },
+
+      )
+    }
+  }
+
+
 
   return (
     <>
       <Container className="mt-5">
+      <Row>
+          <Col>
+            <ToastContainer /> 
+          </Col>
+        </Row>
         <Row>
           <Col>
             <Table striped bordered hover>
@@ -65,7 +117,7 @@ const Profesores = () => {
                       >
                         Modificar
                       </Button>{" "}
-                      <Button variant="danger" size="sm" className="ms-2" onClick={handleEliminar}>
+                      <Button variant="danger" size="sm" className="ms-2" onClick={() => handleEliminar(prof.clave, prof.nombres, prof.apellidos)}>
                         Eliminar
                       </Button>
                     </td>
@@ -77,15 +129,15 @@ const Profesores = () => {
         </Row>
         <Row>
           <Col>
-            <Modal show={show} onHide={handleClose}>
+            <Modal show={show.show} onHide={handleClose}>
               <Modal.Header closeButton>
-                <Modal.Title>Chave-nome</Modal.Title>
+                <Modal.Title>{`${show.clave}-${show.nombre} ${show.apellidos}`}</Modal.Title>
               </Modal.Header>
               <Modal.Body>
                 Tem certeza de que deseja excluir este registro?
               </Modal.Body>
               <Modal.Footer>
-                <Button variant="secondary" onClick={handleClose}>
+                <Button variant="secondary" onClick={() => handleEliminar2(show.clave)}>
                   Fechar
                 </Button>
                 <Button variant="primary" onClick={handleClose}>
