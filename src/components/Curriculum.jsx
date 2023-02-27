020ae87b60acc80ed1f200a060ea0552ae962f41
UriProtocol <angel_3041210085@utd.edu.mx>
2023-02-27 00:29:14 -0600

    curriculum con estilos

--- src/components/Curriculum.jsx
+++ src/components/Curriculum.jsx
@@ -1,93 +1,87 @@
 import React from 'react'
-import {Container, Row, Col} from 'react-bootstrap'
+import { Container, Row, Col } from 'react-bootstrap'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faAngleRight, faAngleLeft, faEnvelope, faPhoneAlt, faSuitcase, faGraduationCap, faLanguage, faComments, faTasks } from '@fortawesome/free-solid-svg-icons';
 
-const Curriculum = ({curriculum}) => {
+const Curriculum = ({ curriculum }) => {
   return (
-    <Container className='border my-4 p-4'>
-            <Row className='mx-auto justify-content-center'>
-              <Row>
-                <h3 className='text-center'>Datos generales</h3>
-              </Row>
-              <Row className='justify-content-center border rounded'>
-                <Row>
-                  <Col><b>Nombre(s): </b><p>{curriculum.infoGeneral.nombres}</p></Col>
-                  <Col><b>Apellido(s): </b><p>{curriculum.infoGeneral.apellidos}</p></Col>
-                  <Col><b>Calle: </b><p>{curriculum.infoGeneral.calle}</p></Col>
-                  <Col><b>Colonia: </b><p>{curriculum.infoGeneral.colonia}</p></Col>
-                </Row>
-                <Row>
-                  <Col><b>Código Postal: </b><p>{curriculum.infoGeneral.cp}</p></Col>
-                  <Col><b>Email: </b><p>{curriculum.infoGeneral.email}</p></Col>
-                  <Col><b>Sexo: </b><p>{curriculum.infoGeneral.sexo}</p></Col>
-                  <Col><b>Fecha de Nacimiento: </b><p>{curriculum.infoGeneral.fNacimiento}</p></Col>
-                </Row>
-                <Row>
-                  <Col><b>Teléfono de Casa: </b><p>{curriculum.infoGeneral.tCasa}</p></Col>
-                  <Col><b>Telefono Celular: </b><p>{curriculum.infoGeneral.tCelular}</p></Col>
-                </Row>
-              </Row>
-            </Row>
-
-            <Row className='mx-auto justify-content-center'>
-              <Row>
-                <h3 className='text-center'>Escolaridades</h3>
-              </Row>
-              <Row className='justify-content-center border rounded'>
-                <Row>
-                  <Col><b>Centro educativo: </b><p>{curriculum.escolaridades.centroEducativo}</p></Col>
-                  <Col xs={6}><b>Ubicación del centro educativo: </b><p>{curriculum.escolaridades.ubicacionCentro}</p></Col>
-                  <Col><b>Título: </b><p>{curriculum.escolaridades.titulo}</p></Col>
-                  
-                </Row>
-                <Row>
-                  <Col><b>Campo de estudio: </b><p>{curriculum.escolaridades.campoEstudio}</p></Col>
-                  <Col><b>Año de Graduación: </b><p>{curriculum.escolaridades.graduacion}</p></Col>
-                </Row>
-              </Row>
-            </Row>
-
-            <Row className='mx-auto justify-content-center'>
-              <Row>
-                <h3 className='text-center'>Experiencia</h3>
-              </Row>
-              <Row className='justify-content-center border rounded'>
-                <Row>
-                  <Col><b>Puesto de trabajo: </b><p>{curriculum.experiencia.puesto}</p></Col>
-                  <Col><b>Empleador: </b><p>{curriculum.experiencia.empleador}</p></Col>
-                  <Col><b>Localidad: </b><p>{curriculum.experiencia.localidad}</p></Col>                 
-                </Row>
-                <Row>
-                  <Col><b>Fecha de Inicio: </b><p>{curriculum.experiencia.fInicio}</p></Col>
-                  <Col><b>Fecha de Salida: </b><p>{curriculum.experiencia.fFinal}</p></Col>
-                </Row>
-                <Row>
-                  <Col><b>Descripción: </b><p>{curriculum.experiencia.descripcion}</p></Col>
-                </Row>
-              </Row>
-            </Row>
-
-            <Row className='mx-auto justify-content-center'>
-              <Row>
-                <h3 className='text-center'>Otros</h3>
-              </Row>
-              <Row className='justify-content-center border rounded'>
-                <Row>
-                  <Col><b>Competencias: </b><p>{curriculum.otros.competencias}</p></Col>
-                  <Col><b>Actividades Extra: </b><p>{curriculum.otros.actExtra}</p></Col>
-                  <Col><b>Pasatiempos: </b><p>{curriculum.otros.hobbies}</p></Col>
-                </Row>
-                <Row>
-                  <Col><b>Cursos: </b><p>{curriculum.otros.cursos}</p></Col>
-                  <Col><b>Redes Sociales: </b><p>{curriculum.otros.redSociales}</p></Col>
-                  <Col><b>Idiomas: </b><p>{curriculum.otros.idiomas}</p></Col>
-                </Row>
-                <Row>
-                  <Col><b>Valores: </b><p>{curriculum.otros.valores}</p></Col> 
-                </Row>
-              </Row>
-            </Row>
-          </Container>
-  )
+    <>
+      <Container className="cv-container my-4 shadow text-start">
+        <Row className='general p-3'>
+          <Col md={6}>
+            <h1>{curriculum.infoGeneral.nombres} {curriculum.infoGeneral.apellidos}</h1>
+            <p><b>Fecha de nacimiento:</b> {curriculum.infoGeneral.fNacimiento}</p>
+            <p><b>Calle y Colonia:</b> {curriculum.infoGeneral.calle}, {curriculum.infoGeneral.colonia}</p>
+            <p><b>Codigo postal:</b> {curriculum.infoGeneral.cp}</p>
+            <p><FontAwesomeIcon icon={faEnvelope} /> {curriculum.infoGeneral.email}</p>
+            <p><FontAwesomeIcon icon={faPhoneAlt} /> {curriculum.infoGeneral.tCelular}</p>
+          </Col>
+        </Row>
+        <Row className="mt-3 p-3">
+          <Col>
+            <h2><FontAwesomeIcon icon={faGraduationCap} /> Educación</h2>
+            <ul>
+              <li>
+                <h4>{curriculum.escolaridades.centroEducativo}</h4>
+                <p><b>Ubicacion del centro educativo: </b>{curriculum.escolaridades.ubicacionCentro}</p>
+                <p><b>Titulo: </b>{curriculum.escolaridades.titulo}</p>
+                <p><b>Campo de estudio: </b>{curriculum.escolaridades.campoEstudio}</p>
+                <p><b>Año de Graduación: </b>{curriculum.escolaridades.graduacion}</p>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+        <Row className="mt-3 p-3">
+          <Col>
+            <h2><FontAwesomeIcon icon={faSuitcase} /> Experiencia profesional</h2>
+            <ul>
+              <li>
+                <h4>Nombre de la empresa</h4>
+                <p><b>Puesto:</b> {curriculum.experiencia.puesto}</p>
+                <p><b>Empleador:</b> {curriculum.experiencia.empleador}</p>
+                <p><b>Localidad:</b> {curriculum.experiencia.localidad}</p>
+                <p><b>Fecha de inicio:</b> {curriculum.experiencia.fInicio}</p>
+                <p><b>Fecha de fin:</b> {curriculum.experiencia.fFinal}</p>
+                <p><b>Descripción:</b> {curriculum.experiencia.descripcion}</p>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+        <Row className="mt-3 p-3">
+          <Col>
+            <h2><FontAwesomeIcon icon={faTasks} /> Datos adicionales</h2>
+            <ul>
+              <li><p><b>Competencias: </b> {curriculum.otros.competencias}</p></li>
+              <li><p><b>Hobbies: </b> {curriculum.otros.hobbies}</p></li>
+              <li><p><b>Cursos: </b> {curriculum.otros.cursos}</p></li>
+              <li><p><b>Actividades Extracurriculares: </b> {curriculum.otros.actExtra}</p></li>
+              <li><p><b>Valores: </b> {curriculum.otros.valores}</p></li>
+            </ul>
+          </Col>
+        </Row>
+        <Row className="mt-3 p-3">
+          <Col>
+            <h2><FontAwesomeIcon icon={faLanguage} /> Idiomas</h2>
+            <ul>
+              <li>
+                <p>{curriculum.otros.idiomas}</p>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+        <Row className="mt-3 p-3">
+          <Col>
+            <h2><FontAwesomeIcon icon={faComments} /> Redes Sociales</h2>
+            <ul>
+              <li>
+                <p>{curriculum.otros.redSociales}</p>
+              </li>
+            </ul>
+          </Col>
+        </Row>
+      </Container>
+    </>
+  );
 }
 
 export default Curriculum
